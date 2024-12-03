75a58b954095bbbb43f29bff57dd4f62838f02cf
Kavinraj <kavinraj_93@yahoo.com>
2024-12-03 17:47:03 +0800

    Modified app.py to deploy faster

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import random
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -85,4 +86,5 @@ def view_assignments():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
